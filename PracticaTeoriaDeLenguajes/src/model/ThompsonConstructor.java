0256a341ca6803a3af2d05aca6891ffbdf1c28e3
Luis <desarrolloingenieria13@udea.edu.co>
2018-09-18 10:31:38 -0500

    unionConstruction terminado y sobre carga para que admita tres o más uniones

--- PracticaTeoriaDeLenguajes/src/model/ThompsonConstructor.java
+++ PracticaTeoriaDeLenguajes/src/model/ThompsonConstructor.java
@@ -36,11 +36,34 @@ public class ThompsonConstructor {
     }
     
     public static DoubleNode[] unionConstruction (String simbol1, String simbol2) {
-        DoubleNode[] vector = new DoubleNode[3];
+        DoubleNode[] vector = new DoubleNode[2];
         DoubleNode consSimbol1 = simpleConstruction(simbol1);
         DoubleNode consSimbol2 = simpleConstruction(simbol2);
         DoubleNode first = new DoubleNode();
         first.link1 = consSimbol1;
-        
+        first.link2 = consSimbol2;
+        DoubleNode last = new DoubleNode();
+        consSimbol1.link1.link1 = last;
+        consSimbol2.link1.link1 = last;
+        vector[0] = first;
+        vector[1] = last;
+        return vector;
+    }
+    
+    public static DoubleNode[] unionConstruction (DoubleNode start, DoubleNode end, String simbol) {
+        DoubleNode[] vector = new DoubleNode[2];
+        DoubleNode consSimbol1 = start.link2;
+        DoubleNode consSimbol2 = simpleConstruction(simbol);
+        DoubleNode first = new DoubleNode();
+        start.link1 = first;
+        first.link1 = consSimbol1;
+        first.link2 = consSimbol2;
+        DoubleNode last = new DoubleNode();
+        last.link1 = end;
+        consSimbol1.link1.link1 = last;
+        consSimbol2.link1.link1 = last;
+        vector[0] = first;
+        vector[1] = last;
+        return vector;
     }
 }
