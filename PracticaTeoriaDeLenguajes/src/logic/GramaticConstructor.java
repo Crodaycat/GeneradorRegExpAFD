417956e48bf9094a25b47803991fc984f709f0ab
Luis <desarrolloingenieria13@udea.edu.co>
2018-11-07 11:52:38 -0500

    Se definen clases para generar gramaticas con el automata

---
+++ PracticaTeoriaDeLenguajes/src/logic/GramaticConstructor.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logic;
+
+import model.AFD;
+import model.Gramatic;
+
+/**
+ *
+ * @author luis_
+ */
+public class GramaticConstructor 
+{
+    public static Gramatic automatomToGramatic (AFD automatom) 
+    {
+        return null;
+    }
+}
